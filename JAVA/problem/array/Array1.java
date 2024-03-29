76ad778ec7e0356b1636d047c84a47cb94733665
injoon <dlswns8679@naver.com>
2024-03-29 08:47:40 -0500

    자바-일일정리

---
+++ JAVA/problem/array/Array1.java
@@ -0,0 +1,10 @@
+package JAVA.problem.array;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int[] students; //배열 변수 선언
+        students = new int[5];
+
+
+    }
+}
---
+++ JAVA/problem/array/Array2.java
@@ -0,0 +1,4 @@
+package JAVA.problem.array;
+
+public class Array2 {
+}
